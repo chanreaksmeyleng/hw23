252e71e8f7294932e13e9a90ea6c9a0f7401110e
chanreaksmeyleng <chanreaksmey.leng@gmail.com>
2025-12-11 17:13:37 +0700

    first commit

--- script.js
+++ script.js
@@ -3,21 +3,31 @@ function login() {
   const passwordElement = document.getElementById("password");
   const email = emailElement.value;
   const password = passwordElement.value;
-  const wrongPasswordElement = document.getElementById("wrongPassword");
+  let wrongPasswordElement = document.getElementById("wrongPassword");
+  let message = "";
   console.log("Email is ", email);
   console.log("password is is ", password);
-  // window.open("./homepage.html", "_self");
 
-  if (password == email) {
-    window.open("./homepage.html", "_self");
-  } else {
-    wrongPasswordElement.innerText = "Invalid credentials. Please try again."
+  if (email != ""){
+    console.log("b")
+    if (password == ""){
+      wrongPasswordElement.innerText = "Password is mandatory";
+    }else{
+      if (email != password){
+        wrongPasswordElement.innerText = "Invalid credentials. Please try again.";
+      }else{
+        window.open("./homepage.html", "_self");
+      }
+    }
+  }else{
+    message = "Email ";
+    if (password == ""){
+      wrongPasswordElement.innerText = message + "and Password are mandatory";
+    }else if(password != ""){
+      wrongPasswordElement.innerText = message + "is mandatory";
+    }
   }
 }
-// const logout = () => {
-//   console.log("abc")
-//   window.open("./index.html", "_self");
-// }
 
 function logout(){
   window.open("./index.html", "_self");
